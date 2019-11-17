c631fb05f7861fefdae59f30cec7bb66de83adda
Grzesiek <grzesiek7393223@o2.pl>
2019-11-17 13:33:37 +0100

    first

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Zjazd_nr1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
